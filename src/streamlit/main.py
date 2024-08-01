d6ad44670daf6afb11e858ad7bbf868ebfadd918
Hitalo Nascimento <hitalobruno2011@gmail.com>
2024-08-01 13:47:05 -0300

    atualizou apresentacao

--- src/streamlit/main.py
+++ src/streamlit/main.py
@@ -23,7 +23,7 @@ st.markdown(
     'Nossa análise pretende identificar padrões e tendências que possam fornecer uma compreensão mais profunda sobre o perfil dos estudantes que ingressaram na UFPE nesses anos. Consideramos fatores como a distribuição geográfica dos ingressantes, a escolha dos campi e dos cursos, bem como a influência das políticas de cotas na composição do corpo discente. Ao explorar essas dimensões, esperamos revelar elementos que possam auxiliar na formulação de políticas educacionais e na melhoria dos processos de admissão da universidade.'
     '</div>'
     '<div style="text-align: justify; margin-bottom: 20px;">'
-    'Utilizaremos a ferramenta Streamlit para apresentar a análise de forma interativa e visualmente atraente. As bibliotecas Python pandas, openpyxl, matplotlib, seaborn, pymongo e python-dotenv serão empregadas para a manipulação dos dados, criação de visualizações, e gerenciamento de ambientes. Com essas metodologias e ferramentas robustas, pretendemos criar uma visão detalhada e integrada dos ingressantes na UFPE, contribuindo para uma gestão mais eficaz e inclusiva da educação superior.'
+    'Utilizaremos a ferramenta Streamlit para apresentar a análise de forma interativa e visualmente atraente. As bibliotecas Python pandas, plotly e seaborn, pymongo e python-dotenv serão empregadas para a manipulação dos dados, criação de visualizações, e gerenciamento de ambientes. Com essas metodologias e ferramentas robustas, pretendemos criar uma visão detalhada e integrada dos ingressantes na UFPE, contribuindo para uma gestão mais eficaz e inclusiva da educação superior.'
     '</div>',
     unsafe_allow_html=True
 )
@@ -32,14 +32,14 @@ st.header("Metodologia")
 
 st.markdown(
     '<div style="text-align: justify; margin-bottom: 20px;">'
-    'A ferramenta escolhida para este projeto foi o Streamlit por diversos motivos. Primeiramente, alguns membros do grupo já possuem experiência com a ferramenta, o que facilita a implementação. Além disso, por ser uma ferramenta gratuita, o Streamlit se torna uma opção econômica e inclusiva. Outro fator importante é que Streamlit é baseado em Python, uma linguagem que todos os integrantes dominam, reduzindo significativamente a curva de aprendizagem.'
+    'Neste projeto, escolhemos utilizar o Streamlit devido a vários fatores. Primeiramente, alguns membros do grupo já possuem experiência com a ferramenta, facilitando sua implementação. Além disso, por ser uma plataforma gratuita, o Streamlit é uma opção econômica e acessível. Outro fator determinante é que o Streamlit é baseado em Python, uma linguagem que todos os integrantes dominam, o que reduz significativamente a curva de aprendizagem.'
     '</div>'
     '<div style="text-align: justify; margin-bottom: 20px;">'
-    'Os dados utilizados neste projeto foram coletados a partir dos registros de ingressantes da UFPE, abrangendo os anos de 2020 a 2024, porém não foram disponibilizados os dados dos semestres 2021.2 e 2024.2. As informações incluídas nos dados são: origem dos aprovados, campus de destino, cursos escolhidos, e a utilização de cotas, incluindo os diferentes tipos de cotas. A limpeza e organização dos dados foram etapas cruciais para garantir a confiabilidade e a precisão das análises. Utilizamos a biblioteca pandas para manipulação dos dados, removendo linhas com informações em branco e excluindo colunas que não eram relevantes para a análise proposta. A biblioteca openpyxl foi utilizada para trabalhar com arquivos csv, facilitando a importação e exportação de dados.'
+    'Os dados analisados foram coletados a partir dos registros de ingressantes da UFPE, abrangendo os anos de 2020 a 2024, com exceção dos semestres 2021.2 e 2024.2. As informações incluídas nos dados envolvem a origem dos aprovados, o campus de destino, os cursos escolhidos e o uso de diferentes tipos de cotas. A limpeza e organização dos dados foram etapas cruciais para garantir a precisão das análises. Utilizamos a biblioteca pandas para manipulação dos dados, removendo registros incompletos e excluindo colunas irrelevantes. A biblioteca openpyxl foi empregada para manipular arquivos CSV, facilitando a importação e exportação de dados.'
     '</div>'
     '</div>'
     '<div style="text-align: justify; margin-bottom: 20px;">'
-    'Para a análise dos dados, utilizamos as bibliotecas matplotlib e seaborn, que nos permitiram criar visualizações detalhadas e intuitivas. Essas visualizações foram essenciais para identificar padrões e tendências nos dados, como a distribuição geográfica dos ingressantes, a escolha dos campi e cursos, e a influência das políticas de cotas.'
+    'Para a análise dos dados, utilizamos as bibliotecas plotly e seaborn, que possibilitaram a criação de visualizações detalhadas e intuitivas. Essas visualizações foram fundamentais para identificar padrões e tendências, como a distribuição geográfica dos ingressantes, a escolha dos campi e cursos, e a influência das políticas de cotas.'
     '</div>',
     unsafe_allow_html=True
 )
